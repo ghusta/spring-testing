574a4460b04867d19315003f472d99f57ddb624d
Guillaume HUSTA <guillaume.husta@douane.finances.gouv.fr>
2018-03-28 11:59:46 +0200

    Migration to Spring Boot 2.0

--- src/test/java/example/HelloE2ESeleniumTest.java
+++ src/test/java/example/HelloE2ESeleniumTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
